67e8b7bf124fc712bb0a00aeb2d0a970c62977ba
Israfil Hossen <sisrafilss@gmail.com>
2021-11-29 21:47:31 +0600

    top projects added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
 import Skills from '../Skills/Skills';
+import TopProjects from '../TopProjects/TopProjects';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Header />
             <Banner />
             <Skills />
+            <TopProjects />
         </div>
     );
 };
---
+++ src/pages/Home/TopProjects/TopProjects.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./TopProjects.css";
+
+const TopProjects = () => {
+  return (
+    <div className="container my-5">
+      <div>
+        <h2 className="display-4 text-light text-center mb-5">
+          Recent Projects
+        </h2>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card">
+            <div className="projects project-1"></div>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                {" "}
+                <h5 class="card-title text-light ">FitPal Bicycle</h5>
+                <Link to="/projects" className="btn btn-success btn-sm">Detail</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <div className="projects project-2"></div>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-light">Horizon Delivery</h5>
+                <Link to="/projects" className="btn btn-success btn-sm">Detail</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <div className="projects project-3"></div>
+            <div class="card-body">
+              <div className="d-flex justify-content-between">
+                <h5 class="card-title text-light">Sundarban Healthcare</h5>
+                <Link to="/projects" className="btn btn-success btn-sm">Detail</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopProjects;
