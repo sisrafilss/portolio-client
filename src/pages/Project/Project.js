52bf8d9f1b2ca5e03b53065a2c3348ea950d1fcc
Israfil Hossen <sisrafilss@gmail.com>
2021-12-01 11:51:38 +0600

    projects page polished

---
+++ src/pages/Project/Project.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const Project = ({ project }) => {
+    
+    const {name, description, images, links} = project;
+
+  return (
+    <div class="col">
+      <div class="card project-card card-bg" id="project">
+        <div
+          className="all-projects"
+          style={{
+            backgroundImage: `url(${images?.image1})`,
+          }}
+        ></div>
+        <div class="card-body">
+          <div>
+            <h5 class="card-title text-center h4"> {name} </h5>
+            <p class="card-text">{description}</p>
+          </div>
+          <div className="d-flex justify-content-between mt-4">
+            <a
+              target="_blank"
+              href={links?.githubClient}
+              class="btn btn-primary"
+            >
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 1024 1024"
+                class="portfolio-icon"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
+              </svg>
+            </a>
+            <a
+              target="_blank"
+              href={links?.liveSite}
+              class="btn btn-primary"
+            >
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 512 512"
+                class="portfolio-icon"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M440 6.5L24 246.4c-34.4 19.9-31.1 70.8 5.7 85.9L144 379.6V464c0 46.4 59.2 65.5 86.6 28.6l43.8-59.1 111.9 46.2c5.9 2.4 12.1 3.6 18.3 3.6 8.2 0 16.3-2.1 23.6-6.2 12.8-7.2 21.6-20 23.9-34.5l59.4-387.2c6.1-40.1-36.9-68.8-71.5-48.9zM192 464v-64.6l36.6 15.1L192 464zm212.6-28.7l-153.8-63.5L391 169.5c10.7-15.5-9.5-33.5-23.7-21.2L155.8 332.6 48 288 464 48l-59.4 387.3z"></path>
+              </svg>
+            </a>
+            <button className="btn btn-primary">Detail</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
